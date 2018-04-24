e4a12c30af3c57866cef569703bc88987d83da69
Kristoffer <kriss0706@hotmail.com>
2018-04-24 18:40:02 +0200

    Initial Release
    - Added sourcecodes
    - Added startup message

---
+++ Github Self/Github Self/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Self
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type !help for a list of commands to use");
+
+            Console.ReadLine();
+        }
+    }
+}
